14282d79dacc23931097ce7e570458b8d2ca7a10
jgeboski <jgeboski@gmail.com>
2022-02-23 22:21:18 -0800

    Version 2.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name="mctl",
-    version="2.0.0",
+    version="2.0.1",
     url="https://github.com/jgeboski/mctl",
     author="James Geboski",
     author_email="jgeboski@gmail.com",
